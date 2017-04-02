9edc3e62372a0fad2b4d44ca93e09a7eeb4a672d
cyming <cy_wangming@163.com>
2017-04-02 18:14:36 +0800

    界面样式

---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+    threeDayUrl: 'https://api.seniverse.com/v3/weather/daily.json',
+    rpoKey: 'bgjuydfs0rqizk5v'
+}
+module.exports = config
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,69 +1,41 @@
 //index.js
 //获取应用实例
+const requestUrl = require('../../config').threeDayUrl
 var util = require('../../utils/util.js')
 var app = getApp()
 Page({
-  data: {
-    results:{},
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
-  },
-  // onLoad:function(){
-  //   var that = this
-  //  wx.getLocation({
-  //    type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
-  //    success: function(res){
-  //      that.setData({
-  //       res:res
-  //     })
-  //     console.log('success')
-  //    },
-  //    fail: function() {
-  //      // fail
-  //      console.log('fail')
-  //    },
-  //    complete: function() {
-  //      // complete
-  //      console.log('complete')
-  //    }
-  //  })
-  // },
+  data:{
+    daily:[],
+    location:{}
+    },
   onLoad:function () {
-    console.log('1')
      var that = this
      wx.request({
-       url: 'https://api.seniverse.com/v3/weather/now.json',
-       data: {
-         key:'bgjuydfs0rqizk5v',
+       url: requestUrl,
+       dataType:'json',
+       data:{
+         key: 'bgjuydfs0rqizk5v',
          location:'shanghai',
          language:'zh-Hans',
-         unit:'c'
+         unit:'c',
+         start:0,
+         days:5
        },
        method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
        header: { 
-         'content-type': 'application/json'}, // 设置请求的 header
+         'content-type': 'application/json'
+         }, // 设置请求的 header
        success: function(res){
          // success
          that.setData({
-           res:res.data.results
-        })
-        console.log(res.data)
+           daily: res.data.results[0].daily
+         })
+         that.setData({
+           location: res.data.results[0].location
+         })
+        console.log('success')
+        // console.log(res.data.results[0].daily)
+        console.log(res.data.results[0].location)
        },
        fail: function() {
          // fail
@@ -74,5 +46,19 @@ Page({
          console.log('complete')
        }
      })
-     }
+    },
+    clickBtn:function(){
+      wx.navigateTo({
+        url: '../test/test',
+        success: function(res){
+          // success
+        },
+        fail: function(res) {
+          // fail
+        },
+        complete: function(res) {
+          // complete
+        }
+      })
+    }
 })
